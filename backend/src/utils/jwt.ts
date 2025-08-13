1715b0d7a62e1b6fef24782c7e8c52b215c583d1
sikadep <andikayamana14045@gmail.com>
2025-08-13 18:51:03 +0700

    refactor: simplify token generation by removing manual expiration handling and using jwt's built-in expiresIn option

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -5,11 +5,10 @@ import moment from "moment-timezone";
 export const generateToken = (user: IUserToken): string => {
   const secretKey = process.env.SECRET_KEY || ""; // ambil SECRET_KEY dari .env
 
-  // Expired 1 jam dari sekarang di zona waktu Jakarta
-  const exp = moment.tz("Asia/Jakarta").add(1, "hours").unix();
-
   // generate token
-  const token = jwt.sign({ ...user, exp }, secretKey);
+  const token = jwt.sign(user, secretKey, {
+    expiresIn: "1h",
+  });
 
   return token;
 };
